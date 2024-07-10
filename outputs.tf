2451b7af085f9982e4af421c76f24ab3b018ce85
Daniele Monti <62102073+Monska85@users.noreply.github.com>
2024-07-10 17:00:24 +0200

    feat: add gitlab_agents_project_id and gitlab_root_namespace_id outputs

--- outputs.tf
+++ outputs.tf
@@ -18,3 +18,13 @@ output "gitlab_agent_kubernetes_context_variables" {
   value       = local.gitlab_agent_kubernetes_context_variables
   description = "The Gitlab Agent information to be used to configure the Kubernetes context."
 }
+
+output "gitlab_agents_project_id" {
+  description = "The ID of the Gitlab project where the Gitlab Agents are installed."
+  value       = data.gitlab_project.this.id
+}
+
+output "gitlab_root_namespace_id" {
+  description = "The ID of the root namespace of the Gitlab Agents project."
+  value       = data.gitlab_group.root_namespace.group_id
+}
