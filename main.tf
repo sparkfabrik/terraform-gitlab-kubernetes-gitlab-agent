dab0c6471a0b1b939ebf22005f90520f63d042c8
Filippo Merante Caparrotta <61194571+filippolmt@users.noreply.github.com>
2025-05-18 16:47:13 +0200

    feat: add gitlab_agent_enabled_deploy variable to control whether GitLab Agent components are deployed (#18)
    * feat: add gitlab_agent_enabled_deploy variable to control whether GitLab Agent components are deployed
    
    * Rename variable
    
    ---------
    
    Co-authored-by: Andrea Panisson <andrea.panisson@sparkfabrik.com>

--- main.tf
+++ main.tf
@@ -129,6 +129,7 @@ resource "kubernetes_secret_v1" "gitlab_agent_token_secret" {
 
 # Helm release
 resource "helm_release" "this" {
+  count            = var.gitlab_agent_deploy_enabled ? 1 : 0
   name             = var.helm_release_name
   repository       = local.helm_chart_url
   chart            = local.helm_chart_name
